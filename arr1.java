8617e063148400c9a17d2f07cf5d5f127840be1d
ROHITSARABU <arulsarabu1719@gmail.com>
2017-06-18 09:29:00 +0100

    sorting

---
+++ arr1.java
@@ -0,0 +1,16 @@
+
+
+	import java.util.Arrays;
+import java.util.*;
+public class arr1 {	
+
+	public static void main(String[] args) {
+		int x[]={2,19,9,17,1,97};
+		System.out.println("before array sort");
+		System.out.println(Arrays.toString(x));
+		Arrays.sort(x);
+		System.out.println("after array sort");
+		System.out.println(Arrays.toString(x));
+		//System.out.println(x);
+	}
+	}
