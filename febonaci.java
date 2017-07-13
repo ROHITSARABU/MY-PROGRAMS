f809d4aeb42c8ba1c92c9334c3881be74af6aefc
ROHITSARABU <arulsarabu1719@gmail.com>
2017-07-13 07:45:48 +0100

    find fibonacci series

---
+++ febonaci.java
@@ -0,0 +1,23 @@
+package sample;
+
+import java.util.Scanner;
+
+public class febonaci 
+{
+public static void main(String[] args) 
+{
+	int n1=0,n2=1,n3,count,i;
+	Scanner sc=new Scanner(System.in);
+	System.out.println("enter the count");
+	count=sc.nextInt();
+	System.out.println(n1+" "+n2);
+	for(i=2;i<count;++i)
+	{
+		n3=n1+n2;
+		System.out.println(" "+n3);
+		n1=n2;
+		n2=n3;
+		
+	}
+}
+}
